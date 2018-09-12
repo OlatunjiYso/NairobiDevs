0444275de149699e505596b60f5204ac6582afbe
OlatunjiYso <olatunjiyso@yahoo.com>
2018-09-12 11:37:26 +0100

    feat(display list of github users): refactor Developers model - create model for developer response - move main and detail activities to views package - setup interface for API that fetch developers - setup dveloperService to consume the API - setup developer presenter that subscribe to the service - connect presenter to developerListAdapter - modify xml file accordingly -[Delivers #155965773]

---
+++ app/src/main/java/com/olatunji/nairobijavadev/view/MainActivity.java
@@ -0,0 +1,30 @@
+package com.olatunji.nairobijavadev.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.olatunji.nairobijavadev.R;
+import com.olatunji.nairobijavadev.presenter.DeveloperPresenter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DeveloperPresenter developerPresenter = new DeveloperPresenter(MainActivity.this);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        developerPresenter.getDevelopers(recyclerView);
+    }
+}
